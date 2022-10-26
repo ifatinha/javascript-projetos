ab6cb55e9a2006f260bfeda424271fdaad7b2b91
Fatima Ferreira <ifatinha99@gmail.com>
2022-10-26 19:50:52 -0300

    Atualizando codigo.

--- exercicio-10/palidromo.js
+++ exercicio-10/palidromo.js
@@ -9,11 +9,7 @@ for (let indice = palavra.length - 1; indice >= 0; indice--) {
 }
 
 if (palavra === novaPalavra) {
-    console.log("Palavra lida da direita para a esquerda " + novaPalavra);
-    console.log("Palavra lida da esquerda para a direita " + palavra);
     console.log(palavra + " == " + novaPalavra + ", logo é palidromo.");
 } else {
-    console.log("Palavra lida da direita para a esquerda " + novaPalavra);
-    console.log("Palavra lida da esquerda para a direita " + palavra);
     console.log(palavra + " != " + novaPalavra + ", logo não é palidromo.");
 }
