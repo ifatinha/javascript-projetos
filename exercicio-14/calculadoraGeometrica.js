d96904745c0e1dbe6e1ea3097fffc713f4cb6501
Fatima Ferreira <ifatinha99@gmail.com>
2022-11-05 19:46:29 -0300

    enunciado

--- exercicio-14/calculadoraGeometrica.js
+++ exercicio-14/calculadoraGeometrica.js
@@ -1,3 +1,18 @@
+/**
+ * ### Calculadora Geométrica
+
+Escreva um programa em javascript para calcular a área de diferentes formas geométricas. 
+O programa deve iniciar com um menu contendo as diferentes opções de cálculas. 
+As opções devem ser:
+
+- área do triângulo: base * altura / 2
+- área do retângulo: base * altura
+- área do quadrado: lado²
+- área do trapézio: (base maior + base menor) * altura / 2
+- área do círculo: pi * raio² (considere pi = 3.14)
+ * 
+ */
+
 import scanner from 'readline-sync';
 
 executar();
