9e070baea076eaf2abc4efda8cfb74ee31f3973c
Fatima Ferreira <ifatinha99@gmail.com>
2022-11-05 18:37:56 -0300

    Melhoria no codigo

--- exercicio-14/calculadoraGeometrica.js
+++ exercicio-14/calculadoraGeometrica.js
@@ -1,8 +1,12 @@
 import scanner from 'readline-sync';
 
-let opcao = "";
+executar();
 
-do {
+/**
+ * Funções do programa
+ * 
+ * */
+function exibirMenu() {
     console.log("Calculadora Geométrica");
     console.log("Escolha uma das opções abaixo");
     console.log("1 - Área do triângulo");
@@ -11,75 +15,80 @@ do {
     console.log("4 - Área do trapézio");
     console.log("5 - Área do círculo");
     console.log("6 - Sair");
-    opcao = scanner.question("Opcao: ");
+    let opcao = scanner.question("Opcao: ");
+    return opcao;
+}
 
-    let base = 0.0;
-    let altura = 0.0;
-    const PI = 3.14;
-    let resultado = 0.0;
-    console.clear();
-    switch (opcao) {
+function executar() {
+    let opcao = "";
+    do {
+        opcao = exibirMenu();
+        const PI = 3.14;
+        let resultado = 0.0;
+        console.clear();
+        switch (opcao) {
 
-        case "1":
-            base = parseFloat(scanner.question("Base do triangulo: "));
-            altura = parseFloat(scanner.question("Altura do triangulo: "));
-            console.log("Calculando área do triângulo...");
-            resultado = areaTriangulo(base, altura);
-            console.log("Área do triângulo: " + resultado.toFixed(2));
-            break
-        case "2":
-            base = parseFloat(scanner.question("Base do retângulo: "));
-            altura = parseFloat(scanner.question("Altura do retângulo: "));
-            console.log("Calculando área do retangulo...");
-            resultado = areaRetangulo(base, altura);
-            console.log("Área do retângulo: " + resultado.toFixed(2));
-            break
-        case "3":
-            let lado = parseFloat(scanner.question("Lado do quadrado: "));
-            console.log("Calculando área do quadrado...");
-            resultado = areaQuadrado(lado, lado);
-            console.log("Área do quadrado: " + resultado.toFixed(2));
-            break
-        case "4":
-            let base_maior = parseFloat(scanner.question("Base maior do trapézio: "));
-            let base_menor = parseFloat(scanner.question("Base menor do trapézio: "));
-            altura = parseFloat(scanner.question("Altura do trapézio: "));
-            console.log("Calculando área do trapézio...");
-            resultado = areaTrapezio(base_maior, base_menor, altura);
-            console.log("Área do trapézio: " + resultado.toFixed(2));
-            break
-        case "5":
-            let raio = parseFloat(scanner.question("Raio do circulo: "));
-            console.log("Calculando area do circulo....");
-            resultado = areaCirculo(PI, raio);
-            console.log("Área do circulo: " + resultado.toFixed(2));
-            break
-        case "6":
-            console.log("Encerrando aplicação.....");
-            break
-        default:
-            console.log("Opção inválida!!!");
-            break
-    }
+            case "1":
+                console.log("Calculando área do triângulo...");
+                resultado = areaTriangulo(base);
+                console.log("Área do triângulo: " + resultado.toFixed(2));
+                break
+            case "2":
+                console.log("Calculando área do retangulo...");
+                resultado = areaRetangulo();
+                console.log("Área do retângulo: " + resultado.toFixed(2));
+                break
+            case "3":
+                console.log("Calculando área do quadrado...");
+                resultado = areaQuadrado();
+                console.log("Área do quadrado: " + resultado.toFixed(2));
+                break
+            case "4":
+                console.log("Calculando área do trapézio...");
+                resultado = areaTrapezio();
+                console.log("Área do trapézio: " + resultado.toFixed(2));
+                break
+            case "5":
+                console.log("Calculando area do circulo....");
+                resultado = areaCirculo(PI);
+                console.log("Área do circulo: " + resultado.toFixed(2));
+                break
+            case "6":
+                console.log("Encerrando aplicação.....");
+                break
+            default:
+                console.log("Opção inválida!!!");
+                break
+        }
 
-} while (opcao !== "6");
+    } while (opcao !== "6");
+}
 
-function areaTriangulo(base, altura) {
+function areaTriangulo() {
+    let base = parseFloat(scanner.question("Base do triangulo: "));
+    let altura = parseFloat(scanner.question("Altura do triangulo: "));
     return (base * altura) / 2;
 }
 
-function areaRetangulo(base, altura) {
+function areaRetangulo() {
+    let base = parseFloat(scanner.question("Base do retângulo: "));
+    let altura = parseFloat(scanner.question("Altura do retângulo: "));
     return (base * altura);
 }
 
-function areaQuadrado(lado) {
-    return areaRetangulo(lado, lado);
+function areaQuadrado() {
+    let lado = parseFloat(scanner.question("Lado do quadrado: "));
+    return (lado * lado);
 }
 
-function areaTrapezio(base_maior, base_menor, altura) {
+function areaTrapezio() {
+    let base_maior = parseFloat(scanner.question("Base maior do trapézio: "));
+    let base_menor = parseFloat(scanner.question("Base menor do trapézio: "));
+    let altura = parseFloat(scanner.question("Altura do trapézio: "));
     return (((base_maior + base_menor) * altura) / 2);
 }
 
-function areaCirculo(valorPi, raio) {
+function areaCirculo(valorPi) {
+    let raio = parseFloat(scanner.question("Raio do circulo: "));
     return (valorPi * (Math.pow(raio, 2)));
 }
